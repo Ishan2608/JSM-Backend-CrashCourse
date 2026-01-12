222bc7ef30cc6a24bab51f9627254f92967415cc
Ishan2608 <ishan.developerguide@gmail.com>
2026-01-12 15:40:15 +0530

    Setup Authorize Middleware

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,11 @@
 import {Router} from 'express';
 import { createUser, deleteUser, getUser, getUsers, updateUser } from '../controllers/user.controller.js';
+import { authorize } from '../middlewares/auth.middleware.js';
 
 const userRouter = Router();
 
 userRouter.get('/', getUsers);
-userRouter.get('/:id', getUser);
+userRouter.get('/:id', authorize, getUser);
 userRouter.post('/', createUser);
 userRouter.put('/:id', updateUser);
 userRouter.delete('/:id', deleteUser);
